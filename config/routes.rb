1907754645573c79bedceddd8c696922918e9837
Diogo <diogopaurelio@gmail.com>
2015-07-07 15:21:03 +0200

    added update action to users controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1,
       constraints: ApiConstraints.new(version: 1, default: true) do #scoped by the url - http://api.marketplace.dev/v1/products/1
-        resources :users, :only => [:show]
+        resources :users, :only => [:show, :create, :update]
     end
   end
 
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -10,8 +10,75 @@ describe Api::V1::UsersController do
     end
     it 'returns the information about a reporter on a hash' do
       user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response[:email].to eql @user.email)
+      expect(user_response[:email]).to eql @user.email
     end
     it { should respond_with 200 }
-  end
+  end #describe
+
+  describe 'POST #create' do
+    context 'when it is successfully created' do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+      it 'renders the json representation for the user record just created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+      it { should respond_with 201 }
+    end #context
+
+    context 'when is not created' do
+      before(:each) do
+        #not including the email
+        @invalid_user_attributes = { password: '12345678',
+                                    password_confirmation: '12345678'  }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+      it 'renders an errors json' do
+        user_response = JSON.parse(response.body, symbolize_names: true )
+        expect(user_response).to have_key(:errors)
+      end
+      it 'renders the json errors on why the user couldnt be created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+      it { should respond_with 422 }
+    end #context
+  end #describe
+
+  describe "PATCH #UPDATE" do
+    context "when it is successfully created" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id, user: { email: "newmail@example.com" } }, format: :json
+      end
+      it 'renders the json representation of the updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newmail@example.com"
+      end
+
+      it { should respond_with 200 }
+    end #context
+
+    context "when it is not created" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id, user: { email: "badmail_example.com" } }, format: :json
+      end
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+      it { should respond_with 422 }
+
+    end #context
+
+  end #describe
+
 end
