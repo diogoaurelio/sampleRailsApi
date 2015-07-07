fc7dbfebae97903a4d0119229bd687e1972bbc0b
Diogo <diogopaurelio@gmail.com>
2015-07-07 11:39:26 +0200

    added validations for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,3 +9,16 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should be_valid }
 end
+
+describe "example validation if email present - result: " do
+  before { @user.email = " " }
+  it { should_not be_valid }
+end
+# after installing gem "shoulda-matchers"
+describe "email & password confirmations: " do
+  #before { @user.email = " " }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+end
