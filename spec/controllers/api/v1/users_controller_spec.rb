173d456b99b5b2ed3535729228050969e3c7354f
Diogo <diogopaurelio@gmail.com>
2015-07-08 15:43:26 +0200

    Added Content Type for server responses

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
-
+#bundle exec rspec spec/controllers/api/v1/users_controller_spec.rb
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
-
+  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1, #{ Mime::JSON }" }
+  before(:each) { request.headers['Content-Type'] = Mime::JSON.to_s }
   describe 'GET #show' do
     before(:each) do
       @user = FactoryGirl.create :user
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id#, format: :json #not needed because Mime added in Request Headers
     end
     it 'returns the information about a reporter on a hash' do
-      user_response = JSON.parse(response.body, symbolize_names: true)
+      user_response = json_response
       expect(user_response[:email]).to eql @user.email
     end
     it { should respond_with 200 }
@@ -19,10 +19,10 @@ describe Api::V1::UsersController do
     context 'when it is successfully created' do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for :user
-        post :create, { user: @user_attributes }, format: :json
+        post :create, { user: @user_attributes }#, format: :json #not needed because Mime added in Request Headers
       end
       it 'renders the json representation for the user record just created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:email]).to eql @user_attributes[:email]
       end
       it { should respond_with 201 }
@@ -33,14 +33,14 @@ describe Api::V1::UsersController do
         #not including the email
         @invalid_user_attributes = { password: '12345678',
                                     password_confirmation: '12345678'  }
-        post :create, { user: @invalid_user_attributes }, format: :json
+        post :create, { user: @invalid_user_attributes }#, format: :json #not needed because Mime added in Request Headers
       end
       it 'renders an errors json' do
         user_response = JSON.parse(response.body, symbolize_names: true )
         expect(user_response).to have_key(:errors)
       end
       it 'renders the json errors on why the user couldnt be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
       it { should respond_with 422 }
@@ -51,10 +51,10 @@ describe Api::V1::UsersController do
     context "when it is successfully created" do
       before(:each) do
         @user = FactoryGirl.create :user
-        patch :update, { id: @user.id, user: { email: "newmail@example.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "newmail@example.com" } }#, format: :json #not needed because Mime added in Request Headers
       end
       it 'renders the json representation of the updated user' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:email]).to eql "newmail@example.com"
       end
 
@@ -64,15 +64,15 @@ describe Api::V1::UsersController do
     context "when it is not created" do
       before(:each) do
         @user = FactoryGirl.create :user
-        patch :update, { id: @user.id, user: { email: "badmail_example.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "badmail_example.com" } }#, format: :json #not needed because Mime added in Request Headers
       end
       it "renders an errors json" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
       it "renders the json errors on why the user could not be created" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response[:errors][:email]).to include "is invalid"
       end
       it { should respond_with 422 }
@@ -84,7 +84,7 @@ describe Api::V1::UsersController do
   describe "DELETE #Destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
-      delete :destroy, { id: @user.id }, format: :json
+      delete :destroy, { id: @user.id }#, format: :json #not needed because Mime added in Request Headers
     end
     it { should respond_with 200 }
 
