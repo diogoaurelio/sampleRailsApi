900e21f09bc072a9c6fbaddec96f724fddc064c2
Diogo <diogopaurelio@gmail.com>
2015-07-11 17:36:33 +0200

    completed CRUD for products

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1,
       constraints: ApiConstraints.new(version: 1, default: true) do #scoped by the url - http://api.marketplace.dev/v1/products/1
-        resources :users, :only => [:show, :create, :update, :destroy]
+        resources :users, :only => [:show, :create, :update, :destroy] do
+          resources :products, :only => [:create, :update, :destroy]
+        end
         resources :sessions, :only => [:create, :destroy]
         resources :products, :only => [:show, :index]
     end
--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -28,6 +28,89 @@ describe Api::V1::ProductsController do
     end
 
     it { should respond_with 200 }
+  end #describe
+
+  describe "POST #create" do
+    context "when is successfully created" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @product_attributes = FactoryGirl.attributes_for :product
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, product: @product_attributes }
+      end
+
+      it "renders the json represnetation for the product record just created" do
+        product_response = json_response
+        expect(product_response[:title]).to eql @product_attributes[:title]
+      end
+
+      it { should respond_with 201 }
+    end #context
+
+    context "when is not created" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @invalid_product_attributes = { title: "Smart tv", price: "Twelve dollars" }
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, product: @invalid_product_attributes }
+      end
+      it "renders an errors json" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+      it "renders the json errors on why the user could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+      it { should respond_with 422 }
+    end #context
+
+  end #describe
+
+  describe "PUT/PATCH #update" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create :product, user: @user
+      api_authorization_header @user.auth_token
+    end
+    context "when updating is successfully done" do
+      before(:each) do
+        patch :update, { user_id: @user.id, id: @product.id, product: { title: "A smart TV dude" }  }
+      end
+      it "renders the json represnetation for the updated product" do
+        product_response = json_response
+        expect(product_response[:title]).to eql "A smart TV dude"
+      end
+      it { should respond_with 200 }
+    end
+    context "when updating is NOT successful" do
+      before(:each) do
+        patch :update, { user_id: @user.id, id: @product.id, product: { price: "something" }}
+      end
+      it "renders json represnetation for errors" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+      it "renders json why product could not be created" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+      it { should respond_with 422 }
+    end
+  end #decribe
+
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create :product, user: @user
+      api_authorization_header @user.auth_token
+    end
+    context "when a product is successfully deleted" do
+      before(:each) do
+        delete :destroy, { user_id: @user.id, id: @product.id }
+      end
+      it { should respond_with 204 }
+    end
   end
 
 end
