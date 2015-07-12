8d40057c9524167e385a0c8c1ff382512a07f11b
Diogo <diogopaurelio@gmail.com>
2015-07-12 19:42:39 +0200

    sort by price

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,5 +20,45 @@ describe Product do
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   it { should validate_presence_of :user_id }
 
+  describe ".filter_by_title" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, title: "A plasma TV"
+      @product2 = FactoryGirl.create :product, title: "Fastest Laptop"
+      @product3 = FactoryGirl.create :product, title: "CD Player"
+      @product4 = FactoryGirl.create :product, title: "LCD TV"
+    end
+    context "when a tv title pattern is sent" do
+      it "returns the 2 products matching" do
+        expect(Product.filter_by_title("TV")).to have(2).items
+      end
+      it "returns the products matching" do
+        expect(Product.filter_by_title("TV").sort).to match_array([@product1, @product4])
+      end
+    end #context
+  end #describe
+
+  describe ".above_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, :price => 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+    it "returns the prices matching" do
+      expect(Product.above_or_equal_to_price(51).sort).to match_array([@product4, @product1, @product3])
+    end
+  end
+
+  describe ".bellow_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, :price => 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+    it "returns the prices matching" do
+      expect(Product.bellow_or_equal_to_price(51).sort).to match_array([@product2])
+    end
+  end
 
 end
